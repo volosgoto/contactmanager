0d625b42a5d97a15ae59fa71985caeb0a9ee6c9c
volosgoto <volosgoto@yahoo.com>
2019-02-14 11:59:32 +0200

    dest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App Component</h1>
+        <Header branding="Contact manager!!!" />
+        <Contact name="John Doe" email="John_Doe@mail.com" phone="333-33-33" />
+        <Contact name="Karl" email="Karl@mail.com" phone="444-44-44" />
+        <Contact name="Sara" email="Sara@mail.com" phone="555-55-55" />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div>
+        <h4>Name: {name}</h4>
+        <li>Email: {email}</li>
+        <li>Phone: {phone}</li>
+      </div>
+    );
+  }
+}
+
+export default Contact;
