5b2ae6d3c8658a4ba0c82ed6e8c2c97e674f1fdd
volosgoto <volosgoto@yahoo.com>
2019-02-15 15:23:11 +0200

    delete contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Consumer } from "../context";
 
 class Contact extends Component {
   state = { showContactInfo: false };
 
   static propTypes = {
-    contact: PropTypes.object.isRequired,
-    deleteClickHendler: PropTypes.func.isRequired
+    contact: PropTypes.object.isRequired
   };
 
   onShowClick = (name, e) => {
@@ -14,36 +14,43 @@ class Contact extends Component {
       showContactInfo: !this.state.showContactInfo
     });
   };
-  onDeleteClick = e => {
-    this.props.deleteClickHendler();
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{" "}
-          <i
-            onClick={this.onShowClick.bind(this, name)}
-            className="fas fa-sort-down"
-            style={{ cursor: "pointer" }}
-          />
-          <i
-            onClick={this.onDeleteClick}
-            className="fas fa-times"
-            style={{ cursor: "pointer", float: "right", color: "black" }}
-          />
-        </h4>
-        {showContactInfo && (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        )}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{" "}
+                <i
+                  onClick={this.onShowClick.bind(this, name)}
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "black" }}
+                />
+              </h4>
+              {showContactInfo && (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              )}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 
 const Context = React.createContext();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+
+    default:
+      return state;
+  }
+};
 
 export class Provider extends Component {
   state = {
@@ -23,7 +37,8 @@ export class Provider extends Component {
         email: "Bob Williamson.com",
         phone: "555-55-55"
       }
-    ]
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   render() {
