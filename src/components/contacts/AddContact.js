5ed55185957e4e8e44c43d7c69ac7268aa42f03f
andrey <volosgoto@yahoo.com>
2019-02-16 15:02:30 +0200

    set state field

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react'
 
 class AddContact extends Component {
-  state = {
-    name: '',
-    email: '',
-    phone: ''
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    let statePropName = e.target.name;
+    this.setState({ statePropName: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+
   }
 
   render() {
-    const { name, email, phone } = this.state;
+
     return (
       <div className="card mb-3">
         <div className="card-header">
           Add Contact
         </div>
+
         <div className="card-body">
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
+                className="form-control form-control-lg"
+                name="textName"
                 type="text"
-                name="name"
-                className="form-control form-control-lg"
-                placeholder="Enter name"
-                value={name}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                className="form-control form-control-lg"
-                placeholder="Enter email"
-                value={email}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="phone">Phone</label>
-              <input
-                type="text"
-                name="phone"
-                className="form-control form-control-lg"
-                placeholder="Enter phone"
-                value={phone}
+                value={this.state.value}
+                onChange={this.handleChange}
               />
             </div>
             <input
@@ -54,7 +54,15 @@ class AddContact extends Component {
           </form>
         </div>
       </div>
-    )
+
+    );
   }
+
+
+
+
+
+
+
 }
 export default AddContact;
